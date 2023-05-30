b06a31cca63d22f09dc3a55cde68e8f0c00e6975
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-05-30 20:40:30 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
 
+start.addEventListener('click', onStartCreateColor);
+stop.addEventListener('click', onStopCreateColor);
+let intervalId = null;
+
+function onStartCreateColor() {
+    start.disabled = true;
+    stop.disabled = false;
+    intervalId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopCreateColor() {
+    start.disabled = false;
+    stop.disabled = true;
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
